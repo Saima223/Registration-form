21f9e82f3dad297353d6028ac92121c00aec1de9
Saima <siddiqui.22062004@gmail.com>
2024-02-27 17:36:11 +0530

    Registration form where user enters his/her details and its saved in firebase and shown.

---
+++ script.js
@@ -0,0 +1,36 @@
+//<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+  import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDJHs3e1zwPGjGy1ZUvtLsoPEp86OYWcoc",
+    authDomain: "fir-integration-2dfea.firebaseapp.com",
+    projectId: "fir-integration-2dfea",
+    storageBucket: "fir-integration-2dfea.appspot.com",
+    messagingSenderId: "162698129212",
+    appId: "1:162698129212:web:645628933281b3c47f3839"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  //get ref to database services
+  const db = getDatabase(app);
+
+  document.getElementById("submit").addEventListener('click', function(e){
+   e.preventDefault();
+   set(ref(db, 'user/' + document.getElementById("username").value),
+   {
+
+     username: document.getElementById("username").value,
+     email: document.getElementById("email").value,
+     password: document.getElementById("pass").value,
+     ConfirmPassword: document.getElementById("conpass").value
+
+   });
+     alert("Login Sucessfull  !");
+  })
+//</script>
